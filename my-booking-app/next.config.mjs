ea1c6cad6034727b19329ce5eeae3d43121c04d9
louaialmasri <louaialmasri9@gmail.com>
2025-10-22 14:42:07 +0200

    zweiter step

---
+++ my-booking-app/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Hier können bei Bedarf Konfigurationen hinzugefügt werden.
+    // Zum Beispiel:
+    // reactStrictMode: true,
+};
+
+export default nextConfig;
